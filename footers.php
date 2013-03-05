409f58c2b48f82ca293c9154db22b90faf37430f
Adrian Verde <dev@adrianverde.com>
2013-03-06 00:34:14 +0100

    Pruebas de lista para partidos
    Luego se puede copiar en la función content_promesas();

--- footers.php
+++ footers.php
@@ -10,22 +10,29 @@ echo <<< EOT
   });
 </script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="{$myv_host}js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 
-        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="{$myv_host}js/vendor/bootstrap.min.js"></script>
 
-        <script src="js/main.js"></script>
+        <script src="{$myv_host}js/main.js"></script>
 EOT;
 }
 function footer_partidos(){
 global $myv_host;
 echo <<< EOT
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="{$myv_host}js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 
-        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="{$myv_host}js/vendor/bootstrap.min.js"></script>
 
-        <script src="js/main.js"></script>
+        <script src="{$myv_host}js/main.js"></script>
+		<script type="text/javascript">
+    $(function () {
+        $("[rel='tooltip']").tooltip({
+		placement: "right"
+		});
+    });
+	</script>
 EOT;
 }
 ?>
