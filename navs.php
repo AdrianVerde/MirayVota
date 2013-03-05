1018e45e91096fdd8357e53682bc938073dd3953
Adrian Verde <dev@adrianverde.com>
2013-03-06 00:50:39 +0100

    Arreglando navs
    aunque vayamos a diseñar nuestras propias navs y no vayamos a usar las
    de bootstrap, tenían los links rotos, así que los he arreglado

--- navs.php
+++ navs.php
@@ -1,32 +1,9 @@
 <?php
 function nav_index(){
-/* echo <<< EOT
-        <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-                    <a class="brand" href="index.php">Mira & Vota</a>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav">
-                            <li class=\"active\"><a href=\"/partidos\">Partidos</a></li>
-                            <li><a href=\"graphs.php\">Gráficas</a></li>
-                            <li><a href=\"parties.php\">Partidos</a></li>
-							<li><a href=\"promises.php\">Promesas</a></li>
-                        </ul>
-						<ul class="nav pull-right">
-                        <li><a href="about.php">About</a></li>
-						</ul>
-                    </div><!--/.nav-collapse -->
-                </div>
-            </div>
-        </div>
-EOT; */
+
 }
 function nav_partidos(){
+global $myv_host;
 echo <<< EOT
         <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="navbar-inner">
@@ -36,13 +13,12 @@ echo <<< EOT
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </a>
-                    <a class="brand" href="index.php">Mira & Vota</a>
+                    <a class="brand" href="{$myv_host}">Mira & Vota</a>
                     <div class="nav-collapse collapse">
                         <ul class="nav">
-                            <li class=\"active\"><a href=\"/partidos\">Partidos</a></li>
-                            <li><a href=\"graphs.php\">Gráficas</a></li>
-                            <li><a href=\"parties.php\">Partidos</a></li>
-							<li><a href=\"promises.php\">Promesas</a></li>
+                            <li class="{$myv_host}active"><a href="{$myv_host}partidos">Partidos</a></li>
+                            <li><a href="{$myv_host}graficas">Gráficas</a></li>
+							<li><a href="{$myv_host}promesas">Promesas</a></li>
                         </ul>
 						<ul class="nav pull-right">
                         <li><a href="about.php">About</a></li>
