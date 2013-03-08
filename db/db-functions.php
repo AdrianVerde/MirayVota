0969da4ae7e494ca7149fae05cecae75b5789273
carloscapote <carloscapote@capocasa.(none)>
2013-03-08 01:06:11 +0100

    Refactorizo funciones -ByParty y corrijo indentación

--- db/db-functions.php
+++ db/db-functions.php
@@ -1,88 +1,80 @@
 <?php
-        require_once(__DIR__ . "/../config/db-config.php");
+	require_once(__DIR__ . "/../config/db-config.php");
 
-        function getParties() {
-       	       return getObject("Parties");
-        }
+	function getParties() {
+		return getObject("Parties");
+	}
 
-        function getPromises() {
-       	       return getObject("Promises");
-        }
+	function getPromises() {
+		return getObject("Promises");
+	}
 
-        function getLists() {
-       	       return getObject("Lists");
-        }
+	function getLists() {
+		return getObject("Lists");
+	}
 
-        function getCandidates() {
-       	       return getObject("Candidates");
-        }
+	function getCandidates() {
+		return getObject("Candidates");
+	}
 
-        function getTopics() {
-       	       return getObject("Topics");
-        }
+	function getTopics() {
+		return getObject("Topics");
+	}
 
 	function getObject($objectName) {
 		$query = "SELECT * FROM $objectName;";
 		return getArrayFromSql($query);
 	}
 
-        function getParty($partyId) {
-        	       if (is_numeric($partyId)) {
-                        $query = "SELECT * FROM Parties WHERE partyId = $partyId";
-                        return getArrayFromSql($query);
-                }
-        }
+	function getParty($partyId) {
+		return getObjectsByParty("Parties", $partyId);
+	}
 
-        function getPromisesByParty($partyId) {
-        	       if (is_numeric($partyId)) {
-                        $query = "SELECT * FROM Promises WHERE partyId = $partyId";
-                        return getArrayFromSql($query);
-                }
-        }
+	function getPromisesByParty($partyId) {
+		return getObjectsByParty("Promises", $partyId);
+	}
 
-        function getListsByParty($partyId) {
-        	       if (is_numeric($partyId)) {
-                        $query = "SELECT * FROM Lists WHERE partyId = $partyId";
-                        return getArrayFromSql($query);
-                }
-        }
-        
-        function getCandidatesByParty($partyId) {
-        	       if (is_numeric($partyId)) {
-                        $query = "SELECT * FROM Candidates WHERE partyId = $partyId";
-                        return getArrayFromSql($query);
-                }
-        }
-        
-        function getTopicsByParty($partyId) {
-        	       if (is_numeric($partyId)) {
-                        $query = "SELECT * FROM Topics WHERE partyId = $partyId";
-                        return getArrayFromSql($query);
-                }
-        }
+	function getListsByParty($partyId) {
+		return getObjectsByParty("Lists", $partyId);
+	}
 
-        function getArrayFromSql($sql) {
-        	       $mysqli = tryConnection();
-        	       
-                $result = $mysqli->query($sql);
-                while($row = $result->fetch_assoc()) {
-					         $array[] = $row;
-					 }
+	function getCandidatesByParty($partyId) {
+		return getObjectsByParty("Candidates", $partyId);
+	}
 
-                $mysqli->close();					 
-                
-					 return $array;
-        }
-        
-        function tryConnection() {
-		        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-		        $mysqli->set_charset("utf8");
-		
-		        if (mysqli_connect_errno()) {
-		                printf("Conexión fallida: %s\n", mysqli_connect_error());
-		                exit();
-		        }
-		        
-		        return $mysqli;
-        }
+	function getTopicsByParty($partyId) {
+		return getObjectsByParty("Topics", $partyId);
+	}
+
+	function getObjectsByParty($objectName, $partyId) {
+		if (is_numeric($partyId)) {
+			$query = "SELECT * FROM $objectName WHERE partyId = $partyId";
+			return getArrayFromSql($query);
+		}
+	}
+
+	function getArrayFromSql($sql) {
+		$mysqli = tryConnection();
+
+		$result = $mysqli->query($sql);
+		while($row = $result->fetch_assoc()) {
+			$array[] = $row;
+		}
+
+		$mysqli->close();
+
+		return $array;
+	}
+
+	function tryConnection() {
+		$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		$mysqli->set_charset("utf8");
+
+		if (mysqli_connect_errno()) {
+			printf("Conexión fallida: %s\n", mysqli_connect_error());
+			exit();
+		}
+
+		return $mysqli;
+	}
 ?>
