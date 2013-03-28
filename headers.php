0cd6eaab1bf8bbcb1d07029d00b5544f5a75ed4f
Adrian Verde <me@adrianverde.com>
2013-03-28 20:01:37 +0100

    Apple touch icons now running for iDevices

--- headers.php
+++ headers.php
@@ -1,13 +1,17 @@
 <?php
+$idevices = "<link rel=\"apple-touch-icon-precomposed\" href=\"touch-icon-iphone.png\" />
+<link rel=\"apple-touch-icon-precomposed\" sizes=\"72x72\" href=\"touch-icon-ipad.png\" />
+<link rel=\"apple-touch-icon-precomposed\" sizes=\"114x114\" href=\"touch-icon-iphone-retina.png\" />
+<link rel=\"apple-touch-icon-precomposed\" sizes=\"144x144\" href=\"touch-icon-ipad-retina.png\" />";
 function header_index(){
-global $myv_host;
+global $myv_host, $idevices;
 echo <<< EOT
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
-
+		{$idevices}
         <link rel="stylesheet" href="{$myv_host}css/bootstrap.min.css">
         <style>
             body {
@@ -22,13 +26,14 @@ echo <<< EOT
 EOT;
 }
 function header_partidos(){
-global $myv_host;
+global $myv_host, $idevices;
 echo <<< EOT
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
+		{$idevices}
 		<link href='{$myv_host}css/font.css' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="{$myv_host}css/bootstrap.min.css">
         <style>
@@ -44,13 +49,14 @@ echo <<< EOT
 EOT;
 }
 function header_promesas(){
-global $myv_host;
+global $myv_host, $idevices;
 echo <<< EOT
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
+		{$idevices}
 		<link href='{$myv_host}css/font.css' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="{$myv_host}css/bootstrap.min.css">
         <style>
